3b59da9f8ddce60bc9b701aef8768c4cd444bc24
igalna <igalna@gmail.com>
2015-06-16 08:21:10 +0100

    Added a static factory constructor for discounts of this style

--- CodeKata/src/kata01/XforYPounds.java
+++ CodeKata/src/kata01/XforYPounds.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+@AllArgsConstructor(staticName="of")
 public class XforYPounds implements Discount {
 
 	private String applyTo;
